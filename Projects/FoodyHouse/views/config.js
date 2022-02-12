814bf4da3abe16d36a9d8375176e7baed715e1ec
john-aja <95049418+john-aja@users.noreply.github.com>
2022-02-13 00:38:38 +0530

    responsive updates
    responsive updates

--- Projects/FoodyHouse/views/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SECONDS = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '2b646d5e-f02e-40b8-8cb1-08b0f20751dd';
-export const MODAL_CLOSE_SEC = 2.5;
